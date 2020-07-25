bfee87b61e865a4259da37fadcb01aa5adc3ce75
liyang <li13526285513@163.com>
2020-07-25 14:05:22 +0800

    增加说明

--- HongMouer.EHR.Models/Enum/EnumSex.cs
+++ HongMouer.EHR.Models/Enum/EnumSex.cs
@@ -11,12 +11,24 @@ namespace HongMouer.EHR.Models
     public enum EnumSex
     {
 
+        /// <summary>
+        /// 未知的性别
+        /// </summary>
         未知的性别 = 0,
 
+        /// <summary>
+        /// 男性
+        /// </summary>
         男性 = 1,
 
+        /// <summary>
+        /// 女性
+        /// </summary>
         女性 = 2,
 
+        /// <summary>
+        /// 未说明的性别
+        /// </summary>
         未说明的性别 = 9
     }
 }
